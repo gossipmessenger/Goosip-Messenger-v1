410191b04fc81bf31b5df437ca33bb9bbc15ae4f
Faisal Sayyed <sayyedfaisal06@gmail.com>
2023-02-12 23:28:02 +0530

    initialized react app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const chats = require('./data/data');
 
 const app = express();
+const PORT = process.env.PORT || 5000
+app.use(cors())
 
-app.listen(5000, () => {
-    console.log('Server started on PORT 5000');
+app.get('/', (req,res) => {
+    res.send('API is running')
+});
+
+app.get('/api/chat', (req,res) => {
+    res.send(chats)
+});
+app.get('/api/chat/:id', (req,res) => {
+    const singleChat = req.params.id
+    const ch = chats.find(chat => {
+        return chat._id === singleChat
+    })
+    res.send(ch)
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
 });
